05d349a33d19d2b167f39d77fd4ad7c0dd740168
putrasatr <satriaputrapratama2002@gmail.com>
2021-03-15 17:14:50 +0700

    commit

--- Kaskus.js
+++ Kaskus.js
@@ -2,16 +2,14 @@ function deretKaskus(n) {
     var arr = []
     for (i = 1; i <= n; i++) {
         var hasil = i * 3
-        if (hasil % 5 === 0 && hasil % 6 === 0)
-            arr.push('KASKUS')
-        else if (hasil % 5 === 0)
-            arr.push('KAS')
-        else if (hasil % 6 === 0)
-            arr.push('KUS')
-        else
-            arr.push(i * 3)
-            
-    } return arr
+        if (hasil % 5 === 0 && hasil % 6 === 0) arr.push('KASKUS')
+        if (hasil % 5 === 0) arr.push('KAS')
+        if (hasil % 6 === 0) arr.push('KUS')
+        else arr.push(i * 3)
+    }
+    return arr
 }
 
 console.log(deretKaskus(10))
+// console.log(deretKaskus(100))
+// console.log(deretKaskus(1))
---
+++ Longest-Shortest-word.js
@@ -0,0 +1,20 @@
+function LongestAndShortestword(str) {
+   const character = str.trim().match(/[a-z 0-9]/gi)
+   const array = character.join('').split(' ')
+   const longest = array.sort((a, b) => b.length - a.length)
+   const result = { 
+      Longest:longest[0],
+      Shortest:longest[longest.length-1] 
+   }
+   return result
+}
+console.log(LongestAndShortestword('fly in The sky   '))
+console.log(LongestAndShortestword('!@#not$ fo#$231und'))
+console.log(LongestAndShortestword(`Enumerating objects: 4, done.
+Counting objects: 100% (4/4), done.
+Delta compression using up to 4 threads
+Compressing objects: 100% (3/3), done.
+Writing objects: 100% (3/3), 541 bytes | 108.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
+To https://github.com/putrasatr/CoderByte.gitdffsdffd
+   0976bc7..011a96f  main -> main`))
