faab2998c0d40e6a994e6932c6676e51c7b9d06e
putrasatr <satriaputrapratama2002@gmail.com>
2021-03-17 17:21:15 +0700

    commit

--- Kaskus.js
+++ Kaskus.js
@@ -4,8 +4,8 @@ function deretKaskus(n) {
     for (let i = 1; i <= n; i++) {
         let hasil = i * 3
         if (hasil % 5 === 0 && hasil % 6 === 0) arr.push('KASKUS')
-        if (hasil % 5 === 0) arr.push('KAS')
-        if (hasil % 6 === 0) arr.push('KUS')
+        else if (hasil % 5 === 0) arr.push('KAS')
+        else if (hasil % 6 === 0) arr.push('KUS')
         else arr.push(i * 3)
     }
     return arr
