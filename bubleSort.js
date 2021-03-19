3c2323b3467bd9317bcf1c69d3e4b8f075195b6f
putrasatr <satriaputrapratama2002@gmail.com>
2021-03-19 17:37:09 +0700

    commit

--- bubleSort.js
+++ bubleSort.js
@@ -1,25 +1,14 @@
 let unsortedArr = [4, 6, 1, 2, 3, 5]
 let unsortedArr2 = [82, 3, 13, 4, 55, 24, 345]
-
-
 let bubbleSort = (inputArr) => {
+    let len = inputArr.length - 1
     inputArr.map(() => {
-        for (
-            let i = 0,
-            j = 1;
-            i < inputArr.length;
-            i++,
-            j++
-        ) {
-            if (inputArr[i] > inputArr[j]) {
-                let tmp = inputArr[i];
-                inputArr[i] = inputArr[j];
-                inputArr[j] = tmp;
-            }
+        for (let i = len; i >= 0; i--) {
+            let j = i - 1;
+            if (inputArr[i] < inputArr[j]) [inputArr[j], inputArr[i]] = [inputArr[i], inputArr[j]]
         }
     })
-    return inputArr;
-};
-
+    return inputArr
+}
 console.log(bubbleSort(unsortedArr)) // return [ 1, 2, 3, 4, 5, 6 ]
 console.log(bubbleSort(unsortedArr2)) // return [ 3,  4,  13, 24, 55, 82, 345 ]
