81d03c0b71039db09da3bffb66588e282ed53ab8
putrasatr <satriaputrapratama2002@gmail.com>
2021-04-22 16:23:30 +0700

    commit

--- bubleSort.ts
+++ bubleSort.ts
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-let unsortedArr = [4, 6, 1, 2, 3, 5]
-let unsortedArr2 = [82, 3, 13, 4, 55, 24, 0, 345, 4, 6, 1, 2, 3, 5, -1, -22]
-let bubbleSort = (arr) => {
-    arr.map(() => {
-        for (let i = arr.length; i >= 0; i--) {
-            let j = i - 1;
-           if(arr[i] < arr[j]) [arr[j], arr[i]] = [arr[i], arr[j]];
-        }
-    })
-    return arr
-}
-
-console.log(bubbleSort(unsortedArr)) // return [ 1, 2, 3, 4, 5, 6 ]
-console.log(bubbleSort(unsortedArr2)) // return ['-22', '-1', 0, 1, 2, 3, 3, 4, 4, 5, 6, 13, 24, 55, 82, 345]
-
-
-//-----------Array method sort------------
-// unsortedArr2.sort((a, b) => a - b);
-// unsortedArr.sort((a, b) => a - b);
-=======
 let unsortedArr = [4, 6, 1, 2, 3, 5]
 let unsortedArr2 = [82, 3, 13, 4, 55, 24, 0, 345, 4, 6, 1, 2, 3, 5, -1, -22]
 let bubbleSort = (arr: any) => {
@@ -38,5 +17,4 @@ console.log(bubbleSort(unsortedArr2)) // return ['-22', '-1', 0, 1, 2, 3, 3, 4, 
 //-----------Array method sort------------
 // unsortedArr2.sort((a, b) => a - b);
 // unsortedArr.sort((a, b) => a - b);
->>>>>>> 15056a9125119a20f0aa10d2d7954608a120ba23
 // console.log(unsortedArr,unsortedArr2)
---
+++ regex.js
@@ -0,0 +1,40 @@
+const { mesinHitung } = require('./Batch24/Challenge/Challenge17/challenge17')
+let mh = new mesinHitung
+
+
+function loopWithoutFor(s) {
+    let letter = s.match(/[a-z]/g).join('')
+    let a = letter.match(/[aiueo]/g).join('')
+    let b = letter.match(/[^aiueoAIUEO]/g).join('')
+    let i = 0
+    // console.log(!a)
+    // do {
+    //     console.log((a + b)[i])
+    //     i++ 
+    // } while (i != (a + b).length);
+}
+
+loopWithoutFor('[javascriptz ')
+
+
+function Calculator(n) {
+    let d = /(\d)/g
+    let D = /\D/g
+
+    console.log(Number(n.match(d)))
+    console.log(n.split(D)[0])
+
+    // for (let i = 0; i < n.length; i++) {
+    //     if(n[i]){}
+
+    // }
+    // mh.tambah(3)
+    // console.log(mh.tambah(6).bagi(3).x)
+
+    let array = [n[0], n[1], n[2]]
+    // let result = n.match(d).join('') + n.match(D).join('')
+    // return a
+}
+Calculator('17+1-2+5')
+
+// console.log()
