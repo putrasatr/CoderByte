ce2c7666088fb5197fa295b1c357392e6500e950
putrasatr <satriaputrapratama2002@gmail.com>
2021-07-05 17:13:21 +0700

    timesince

---
+++ timeSince.js
@@ -0,0 +1,28 @@
+const moment = require("moment")
+function timeSince(date) {
+
+  var seconds = Math.floor((new Date() - date) / 1000);
+  console.log('s', seconds)
+  var interval = seconds / 31536000;
+
+  if (interval > 1) {
+    return Math.floor(interval) + " years";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + "month ";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + " d";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + " h";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " mi";
+  }
+  return Math.floor(seconds) + " d";
+}
